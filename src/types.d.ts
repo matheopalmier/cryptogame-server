2365383530f8db89eac8d56f56eb127085be59f8
matheopalmier <mpalmier@axome.com>
2025-04-07 13:31:32 +0200

    test

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+// Global declarations to fix TypeScript errors
+declare module 'express';
+declare module 'jsonwebtoken';
+declare module 'bcrypt';
+declare module 'mongoose';
+declare module 'cors';
+declare module 'dotenv';
+declare module 'helmet';
+declare module 'morgan';
+declare module 'express-validator'; 
